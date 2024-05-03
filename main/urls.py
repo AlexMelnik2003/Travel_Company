29d49d44d3f4583e474b89e51e28fbcf51e0e770
AlexMelnik2003 <alexz.melnik.02@gmail.com>
2024-05-03 21:12:23 +0600

    1.4

--- main/urls.py
+++ main/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
                   path('profile/', views.profile, name='profile'),
                   path('logout/', views.user_logout, name='logout'),
                   path('tour/<int:pk>/book/', views.book_tour, name='book_tour'),
-                  path('make_payment/', views.make_payment, name='make_payment'),
+                  path('make_payment/<int:pk>/', views.make_payment, name='make_payment'),
                   path('payment_success/', views.payment_success, name='payment_success'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
